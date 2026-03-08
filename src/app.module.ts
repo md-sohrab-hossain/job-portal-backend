ff757534b89f8d1c9aa31a41076bf2c5d02d9256
sohrab <md.soharubhossen@gmail.com>
2026-03-08 17:21:12 +0600

    init module and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserService } from './user/user.service';
+import { CompanyModule } from './company/company.module';
+import { UserModule } from './user/user.module';
+import { JobModule } from './job/job.module';
+import { ApplicationModule } from './application/application.module';
+import { ApplicationService } from './application/application.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    CompanyModule,
+    UserModule,
+    JobModule,
+    ApplicationModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, UserService, ApplicationService, PrismaService],
 })
 export class AppModule {}
